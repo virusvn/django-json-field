b46d9eb79ffd6a687f0a736d0baa7dcb6c146c93
Nhan Nguyen <nxtnhan@gmail.com>
2018-07-05 17:45:33 +0900

    fix missing field

--- json_field/forms.py
+++ json_field/forms.py
@@ -2,6 +2,8 @@ try:
     import json
 except ImportError:  # python < 2.6
     from django.utils import simplejson as json
+
+from django.forms import fields
 try:
     from django.forms import utils as util
 except ImportError:
